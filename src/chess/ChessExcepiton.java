d53b5114b2ac191e0ef60b69ad4d3cc965ebd0dd
Rafael Abreu <rafael_sabreu@hotmail.com>
2022-11-06 14:34:37 -0300

    ChessException and ChessPosition

---
+++ src/chess/ChessExcepiton.java
@@ -0,0 +1,12 @@
+package chess;
+
+public class ChessExcepiton extends RuntimeException{
+	private static final long serialVersionUID = 1L;
+
+	public ChessExcepiton(String msg) {
+		super(msg);		
+	}
+	
+	
+
+}
